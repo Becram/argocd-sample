177444413ad004fdf7a95820080db29ad995204c
becram <itsbecram@gmail.com>
2021-11-17 20:53:59 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 set -xe 
 ARGOCD_APP_NAME=$1
 
-pushd sysapps/overlays/dev/$ARGOCD_APP_NAME
-helm dependency build ../../../helm_base/$ARGOCD_APP_NAME
-helm template  ../../../helm_base/$ARGOCD_APP_NAME --name-template $ARGOCD_APP_NAME -f values-override.yaml --include-crds > ../../../helm_base/$ARGOCD_APP_NAME/all.yml
+pushd sysapps/overlays/dev/"$ARGOCD_APP_NAME"
+helm dependency build ../../../helm_base/"$ARGOCD_APP_NAME"
+helm template  ../../../helm_base/"$ARGOCD_APP_NAME" --name-template "$ARGOCD_APP_NAME" -f values-override.yaml --include-crds > ../../../helm_base/"$ARGOCD_APP_NAME"/all.yml
 # cat ../../../helm_base/$ARGOCD_APP_NAME/all.yml
-kubectl apply -k .
-rm -fv ../../../helm_base/$ARGOCD_APP_NAME/all.yml
-exit 0
+# kubectl apply -k .
+# rm -rf   ../../../helm_base/$ARGOCD_APP_NAME/all.yml ../../../helm_base/$ARGOCD_APP_NAME/charts ../../../helm_base/$ARGOCD_APP_NAME/Chart.lock
 popd
+exit 0
 set +x
