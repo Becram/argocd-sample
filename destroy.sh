a9661e34d12f5b72aca76c1c62dcd08e7ef35adb
becram <itsbecram@gmail.com>
2021-11-19 22:25:48 +0700

    update

---
+++ destroy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+set -xe 
+ARGOCD_APP_NAME=$1
+
+pushd sysapps/overlays/dev/"$ARGOCD_APP_NAME"
+helm dependency build ../../../helm_base/"$ARGOCD_APP_NAME"
+helm template  ../../../helm_base/"$ARGOCD_APP_NAME" --namespace "$ARGOCD_APP_NAME" --name-template "$ARGOCD_APP_NAME" -f values-override.yaml --include-crds > ../../../helm_base/"$ARGOCD_APP_NAME"/all.yml && kustomize build
+# cat ../../../helm_base/$ARGOCD_APP_NAME/all.yml
+kubectl delete -k .
+rm -rf   ../../../helm_base/$ARGOCD_APP_NAME/all.yml ../../../helm_base/$ARGOCD_APP_NAME/charts ../../../helm_base/$ARGOCD_APP_NAME/Chart.lock
+popd
+exit 0
+set +x
